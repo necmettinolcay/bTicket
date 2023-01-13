9d2998974bebc59bf75b44317d321d14c0b3c164
necmettin.ozer <necmettin.ozer@DESKTOP-FHK1K5I>
2023-01-14 00:27:49 +0300

    Add project files.

---
+++ BusTicket.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BusTicket.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
